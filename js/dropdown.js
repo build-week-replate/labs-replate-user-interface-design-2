fc952502cdd0cc7a09f49d7db88f4892ddb1eefd
Kayla <6lackmacaroon@gmail.com>
2019-04-17 18:59:04 -0600

    dropdown styled - still need to get it under the hamburger

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,24 +1,23 @@
 class Navbar {
       constructor(element) {
             this.element = element;
-            console.log("element", element);
+            // console.log("element", element);
             this.button = this.element.querySelector('.hamburger');
-            console.log("button", this.button);
+            // console.log("button", this.button);
             this.content = this.element.querySelector('.dropdown-content');
             // Add a click handler to the button reference and call the toggleContent method.
-            console.log("content", this.content);
+            // console.log("content", this.content);
             this.button.addEventListener('click', () => this.dropdown());
             
       }
     
       dropdown() {
             this.content.classList.toggle('dropdown-hidden');
-            console.log('click');
+            // console.log('click');
       }
 }
 
-let navbars = document.querySelectorAll('.dropdown').forEach( navbar => {console.log(navbar) 
-      new Navbar(navbar)});
+let navbars = document.querySelectorAll('.dropdown').forEach( navbar => {new Navbar(navbar)});
 
 
 
